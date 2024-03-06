5982376e164ffc7c5aaa60836f71dbaee169c8c2
丁壮壮 <DingZhuang0210@163.com>
2024-03-06 17:26:15 +0800

    服务调用与负载均衡

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/cloud/PaymentMain8002.java
@@ -0,0 +1,18 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import tk.mybatis.spring.annotation.MapperScan;
+
+
+@SpringBootApplication
+@MapperScan("com.atguigu.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+@EnableDiscoveryClient
+@RefreshScope // 动态刷新
+public class PaymentMain8002 {
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentMain8002.class, args);
+  }
+}
